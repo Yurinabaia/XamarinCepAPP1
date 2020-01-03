04649d0b020facb8ae6659370a7c684e33a65499
Yuri Nabaia Duarte <yuri_duarte@hotmail.com.br>
2020-01-03 20:01:29 -0300

    Apk pronto

---
+++ Correios/Correios/MainPage.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Correios.Servicos.Modelo;
+using Correios.Servicos;
+
+namespace Correios
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            BOTAO.Clicked += BuscarCep;//Aqui temos a seleção do botão junto com a variavel BOTAO criada o XAML 
+        }
+        private void BuscarCep(Object sender, EventArgs args) //Todo botão tem este formato no metodo, tando o Object e o EventArgs
+        {
+            //Todos botão tem validação e a logica do programa
+            try
+            {
+                string cep = TEXTO.Text.Trim();
+                if (ValidacaoDoCep(cep))
+                {
+                    Endereco end = ViaCepServico.BuscarEnderecoViaCep(cep);
+                    if (end != null)
+                    {
+                        RESULT.Text = "Endereço: " + end.localidade + " " + end.uf + " " + end.bairro + " " + end.logradouro;
+
+                    }
+                    else
+                    {
+                        DisplayAlert("Error", "Cep não existe", "OK");
+                    }
+                }
+            }
+            catch(Exception e) 
+            {
+                DisplayAlert("Error critico", e.Message , "ok");
+            }
+
+        }
+        private bool ValidacaoDoCep(string cep) 
+        {
+            int NovoCep = 0;
+            bool valido = true;
+            if (!int.TryParse(cep, out NovoCep))
+            {
+                DisplayAlert("Error", "Cep dever conter apenas números", "ok");
+
+                valido = false;
+            }
+            else if (cep.Length != 8)
+            {
+                DisplayAlert("Error", "Cep invalido pois o cep tem que conter 8 caracteres", "ok");
+                valido = false;
+            }
+
+
+
+            return valido;
+        }
+    }
+}
---
+++ Correios/Correios/Servicos/ViaCepServico.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using Correios.Servicos.Modelo;
+using Newtonsoft.Json;
+
+namespace Correios.Servicos
+{
+   public class ViaCepServico
+    {
+        private static string EnderecoUrl = "https://viacep.com.br/ws/{0}/json/";
+        public static Endereco BuscarEnderecoViaCep (string cep) 
+        {
+            string NovoEndereco = string.Format(EnderecoUrl, cep);
+            WebClient wc = new WebClient();
+            string conteudo = wc.DownloadString(NovoEndereco);
+            Endereco end = JsonConvert.DeserializeObject<Endereco>(conteudo);
+            if (end.cep == null) return null;
+            return end;
+        }
+    }
+}
